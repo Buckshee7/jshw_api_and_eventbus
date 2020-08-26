8096891a3feab5a9516259c1ab3ae5394951512f
ben <benfd@outlook.com>
2020-08-26 18:38:30 +0100

    Extension 1 done - Select instead of list + change event

--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -1,19 +1,31 @@
 <template>
     <ul>
-        <li v-for="(character, index) in characters" :key="index">
-            <character-select :character="character"></character-select>
-        </li>
+        <select v-on:change="displayDetails" v-model="selectedCharacter">
+            <option disabled>Choose a character</option>
+            <option v-for="(character, index) in characters" :key="index" :value="character">{{ character.name }}</option>
+        </select>
     </ul>
 </template>
 
 <script>
 import CharacterSelect from './CharacterSelect.vue';
+import { eventBus } from '../main.js';
 
 export default {
     name: 'character-list',
+    data(){
+        return{
+            selectedCharacter: null
+        }
+    },
     props: ['characters'],
     components: {
         'character-select': CharacterSelect
+    },
+    methods: {
+        displayDetails: function() {
+            eventBus.$emit('display-details', this.selectedCharacter)
+        }
     }
 }
 </script>
